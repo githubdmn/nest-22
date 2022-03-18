eff6c7c5601c9a52e661bb78b5bcb095228c5de8
githubdmn <dmninbox@gmail.com>
2022-03-18 16:03:55 +0100

    Decorators: Controller Get Post

--- src/section03/section03.controller.ts
+++ src/section03/section03.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/section03')
 export class Section03Controller {
+  @Get('/')
+  index(): string {
+    return 'Section 03 index';
+  }
+
   @Get('/messages')
   listMessages(): string {
     return 'Get messages';
