69b2d0791059933c08e39604b37221eb2f14693d
githubdmn <dmninbox@gmail.com>
2022-03-21 18:44:48 +0100

    added MessageService, MessageRepository

--- src/section05/section05.module.ts
+++ src/section05/section05.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MessageRepository } from './repository';
 import { Section05Controller } from './section05.controller';
+import { MessageService } from './service';
 
 @Module({
-  controllers: [Section05Controller]
+	controllers: [Section05Controller],
+	providers: [MessageService, MessageRepository],
 })
 export class Section05Module {}
