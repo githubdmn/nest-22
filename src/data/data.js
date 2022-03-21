37beba12e757f433336a1003ad013cfa2f72a8e2
githubdmn <dmninbox@gmail.com>
2022-03-21 18:43:51 +0100

    data

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+[
+	{
+		id: 9,
+		content:
+			'Pariatur Lorem cupidatat qui et eu ea minim incididunt consectetur reprehenderit laborum exercitation. Est incididunt quis amet laboris est aliquip anim labore quis ad nulla qui.',
+	},
+	{
+		id: 1,
+		content:
+			'Sint mollit eiusmod aute nostrud duis ex ad. Occaecat excepteur non enim commodo qui aliquip consequat duis magna in.',
+	},
+	{
+		id: 2,
+		content:
+			'Fugiat duis culpa ad dolore exercitation pariatur aute mollit incididunt occaecat do aute non excepteur. Sunt consequat tempor consequat est amet enim.',
+	},
+	{
+		id: 3,
+		content:
+			'Dolor ex Lorem excepteur irure occaecat dolor cupidatat aliqua officia esse. Commodo quis adipisicing qui in exercitation enim aliqua do ullamco elit.',
+	},
+	{
+		id: 7,
+		content:
+			'Anim quis est cupidatat fugiat proident minim veniam occaecat. Aliquip reprehenderit consequat velit tempor sint proident voluptate et magna.',
+	},
+	{
+		id: 6,
+		content:
+			'Est minim proident velit proident magna culpa eiusmod reprehenderit in sunt excepteur dolor. Aliqua culpa eu consequat cillum commodo culpa adipisicing eu aliqua mollit ad sit.',
+	},
+];
