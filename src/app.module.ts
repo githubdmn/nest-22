ad1c0d2119abb7150bc014a5bc74058ff41eacb3
githubdmn <dmninbox@gmail.com>
2022-03-21 18:59:02 +0100

    boilerplate code moved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { Section05Module } from './section05/section05.module';
 
 @Module({
   imports: [Section05Module],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
