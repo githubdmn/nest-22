ca243ae0e689726220ae9830ace72665139afaee
githubdmn <dmninbox@gmail.com>
2022-03-21 18:45:24 +0100

    get-all, get-on, create

--- src/section05/section05.controller.ts
+++ src/section05/section05.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	NotFoundException,
+} from '@nestjs/common';
+import { CreateMessageDTO } from './dto';
+import { MessageService } from './service';
 
-@Controller('section05')
-export class Section05Controller {}
+@Controller('section05/message')
+export class Section05Controller {
+	constructor(public ms: MessageService) {}
+
+	@Get('/all')
+	listMessages(): any {
+		return this.ms.findAll();
+	}
+
+	@Get('/:id')
+	async getMessage(@Param('id') id: number) {
+		const message = await this.ms.findOne(id);
+		if (!message) throw new NotFoundException('Message not found');
+		return message;
+	}
+
+	@Post('/')
+	createMessage(@Body() body: CreateMessageDTO) {
+		return this.ms.create(body.content);
+	}
+}
