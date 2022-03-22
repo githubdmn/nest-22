c825a894d5929064630a2e475f976b6a4f02ca4b
githubdmn <dmninbox@gmail.com>
2022-03-22 12:39:55 +0100

    Imported PowerModule

--- src/section06/cpu/cpu.module.ts
+++ src/section06/cpu/cpu.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from '../power/power.module';
 import { CpuService } from './cpu.service';
 
 @Module({
-  providers: [CpuService]
+	providers: [CpuService],
+	imports: [PowerModule],
 })
 export class CpuModule {}
