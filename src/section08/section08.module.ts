52628ada65416ea96cb7688eaade8afd98842086
githubdmn <dmninbox@gmail.com>
2022-03-24 09:53:14 +0100

    database connection - sqlite

--- src/section08/section08.module.ts
+++ src/section08/section08.module.ts
@@ -3,9 +3,19 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Section08Controller } from './section08.controller';
 import { Section08Service } from './section08.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-	imports: [UsersModule, ReportsModule],
+	imports: [
+		UsersModule,
+		ReportsModule,
+		TypeOrmModule.forRoot({
+			type: 'sqlite',
+			database: 'nest.section08',
+			entities: [],
+			synchronize: true,
+		}),
+	],
 	controllers: [Section08Controller],
 	providers: [Section08Service],
 })
