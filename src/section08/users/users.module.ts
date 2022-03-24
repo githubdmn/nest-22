754a6b505c85aa8b8c1b8276ec45dcc7da99d062
githubdmn <dmninbox@gmail.com>
2022-03-24 10:05:05 +0100

    connected user entity to its parent module

--- src/section08/users/users.module.ts
+++ src/section08/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import User from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+	controllers: [UsersController],
+	providers: [UsersService],
+	exports: [User],
+	imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
