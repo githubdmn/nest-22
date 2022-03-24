c6bf42268592f83f3d2ca6274feca26b0129edb7
githubdmn <dmninbox@gmail.com>
2022-03-24 10:05:54 +0100

    connected user entity to its parent module

--- src/section08/users/users.module.ts
+++ src/section08/users/users.module.ts
@@ -7,7 +7,6 @@ import { UsersService } from './users.service';
 @Module({
 	controllers: [UsersController],
 	providers: [UsersService],
-	exports: [User],
 	imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
