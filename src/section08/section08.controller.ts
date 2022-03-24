b10b2e81d62fa377f121808467ff207bb8426455
githubdmn <dmninbox@gmail.com>
2022-03-24 09:50:10 +0100

    generated section08 module controller service

---
+++ src/section08/section08.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('section08')
+export class Section08Controller {}
--- src/section08/section08.module.ts
+++ src/section08/section08.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { Section08Controller } from './section08.controller';
+import { Section08Service } from './section08.service';
 
 @Module({
 	imports: [UsersModule, ReportsModule],
+	controllers: [Section08Controller],
+	providers: [Section08Service],
 })
 export class Section08Module {}
