4dc6038fdc3a4087bcd673329765da9e26591ab8
githubdmn <dmninbox@gmail.com>
2022-03-24 13:02:53 +0100

    userd UserDTO validation in controler

--- src/section08/users/users.controller.ts
+++ src/section08/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUser } from './dtos';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+	@Post('/signup')
+	createUser(@Body() body: CreateUser) {
+		console.log(body);
+	}
+}
