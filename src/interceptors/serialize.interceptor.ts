392552f21b3923bbd681c9fe7e60f0b149ee63af
githubdmn <dmninbox@gmail.com>
2022-03-25 12:51:30 +0100

    excludeExtraneousValues exposes the properties with @Expose() from UserDto

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,17 +2,18 @@ import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import UserDto from 'src/section10/users/dtos/user.dto';
 
 export default class SerializeInterceptor implements NestInterceptor {
 	intercept(
 		context: ExecutionContext,
 		handler: CallHandler,
 	): Observable<any> | Promise<Observable<any>> {
-		console.log('Before handler: ', context);
 		return handler.handle().pipe(
 			map((data: any) => {
-				console.log('Before the response in sent out: ', data);
-				// plainToClass
+				return plainToClass(UserDto, data, {
+					excludeExtraneousValues: true,
+				});
 			}),
 		);
 	}
