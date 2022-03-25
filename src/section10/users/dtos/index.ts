d9a340c7e07fcc2536e53bea18ea84910ebcf7bb
githubdmn <dmninbox@gmail.com>
2022-03-25 12:53:24 +0100

    UserDto for "public use"

--- src/section10/users/dtos/index.ts
+++ src/section10/users/dtos/index.ts
@@ -1,4 +1,5 @@
 import CreateUserDto from './create-user.dto';
 import UpdateUserDto from './update-user.dto';
+import UserDto from './user.dto';
 
-export { CreateUserDto, UpdateUserDto };
+export { CreateUserDto, UpdateUserDto, UserDto };
