3c4ad314bc90d5f4274e03b7620e28a6e3b18c09
githubdmn <dmninbox@gmail.com>
2022-03-25 12:22:24 +0100

    User contorller

---
+++ src/section10/users/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	NotFoundException,
+	Param,
+	Patch,
+	Post,
+	Query,
+} from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from './dtos';
+import { UsersService } from './users.service';
+
+@Controller('/auth')
+export class UsersController {
+	constructor(private userService: UsersService) {}
+	@Post('/signup')
+	createUser(@Body() body: CreateUserDto) {
+		return this.userService.create(body.email, body.password);
+	}
+
+	@Get('/:id')
+	async findUser(@Param('id') id: string) {
+		const user = await this.userService.findById(parseInt(id));
+		if (!user) throw new NotFoundException('User not found');
+		return user;
+	}
+
+	@Get()
+	findAllUsers(@Query('email') email: string) {
+		return this.userService.findByEmail(email);
+	}
+
+	@Delete('/:id')
+	removeUser(@Param('id') id: string) {
+		return this.userService.remove(parseInt(id));
+	}
+
+	// needs new dto
+	@Patch('/:id')
+	updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+		return this.userService.update(parseInt(id), body);
+	}
+}
