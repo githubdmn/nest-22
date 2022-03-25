7da58f2040d906b4afc129efef7535cadf5aa4aa
githubdmn <dmninbox@gmail.com>
2022-03-25 12:20:15 +0100

    report entity

---
+++ src/section10/reports/report.entity.ts
@@ -0,0 +1,9 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export default class Report {
+	@PrimaryGeneratedColumn()
+	id: number;
+	@Column()
+	price: number;
+}
