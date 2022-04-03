1bd30529303bb83338ea3d7f45f21070307dd2c3
githubdmn <dmninbox@gmail.com>
2022-04-03 10:24:46 +0200

    FIX: require(cookie-session)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import cookieSession from 'cookie-session'; // Unhandled promises
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(
+		cookieSession({
+			keys: ['cookie-session'],
+		}),
+	);
 	app.useGlobalPipes(
 		new ValidationPipe({
 			whitelist: true,
