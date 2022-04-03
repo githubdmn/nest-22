3aa02cf21400e6be3dc043b8fd05f8c1afeaab34
githubdmn <dmninbox@gmail.com>
2022-04-03 21:13:04 +0200

    wire up CurrentUserInterceptor

--- src/section11/users/users.module.ts
+++ src/section11/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import User from './user.entity';
+import { CurrentUserInterceptor } from './interceptors';
 
 @Module({
 	controllers: [UsersController],
-	providers: [UsersService, AuthService],
+	providers: [UsersService, AuthService, CurrentUserInterceptor],
 	imports: [TypeOrmModule.forFeature([User])],
 })
-export class UsersModule {}
+export class UsersModule { }
